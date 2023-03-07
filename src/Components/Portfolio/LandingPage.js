b3af642923e6e717bfa92ef4b831da1df31b0d0d
nahid770 <nurnahid10@gmail.com>
2023-03-07 10:47:33 +0600

    project added

--- src/Components/Portfolio/LandingPage.js
+++ src/Components/Portfolio/LandingPage.js
@@ -3,14 +3,19 @@ import { BiLinkExternal } from 'react-icons/bi';
 // import doctor from '../../Images/landing.png'
 import landing1 from '../../Images/landing1.png'
 import landing2 from '../../Images/luxury.png'
+import landing3 from '../../Images/landing3.png'
+import booking from '../../Images/booking.png'
 import './LandingPage.css'
 
 
 const LandingPage = () => {
     return (
-            <div className='md:max-w-[1024px] md:flex  gap-10'>
-                {/* Landing page 1 */}
-               <div className='container relative '>
+            <div className=' flex flex-col md:flex-row  justify-center items-center mx-16'>
+
+            
+
+                        {/* Landing page 1 */}
+               <div className='container relative'>
                 <div className='image-wrapper-l  mx-auto shadow-md rounded-sm'>
                     <img className='image  overflow-hidden ' src={landing1} alt="" />
                      <div className="middle">
@@ -21,7 +26,7 @@ const LandingPage = () => {
                         </div>
                     <div className='overley absolute '></div>
                 </div>
-                <div className='md:w-full w-2/3 mx-auto'>
+                <div className=' w-2/3 mx-auto'>
                 <p className='text-white font-popins text-center py-1 mt-2 text-g '>NFT Landing Page</p>
                 </div>
                </div>
@@ -38,10 +43,46 @@ const LandingPage = () => {
                         </div>
                     <div className='overley absolute '></div>
                 </div>
-                <div className='md:w-full w-2/3 mx-auto'>
-                    <p className='text-white font-popins text-center py-1 mt-2 text-g '>Interior Design Landing Page</p>
+                <div className=' w-2/3 mx-auto'>
+                    <p className='text-white font-popins text-center py-1 mt-2 text-g '>Interior Design Landing</p>
                 </div>
                </div>
+               {/* Landing Page 3 */}
+               <div className='container relative mt-10 md:mt-0'>
+                <div className='image-wrapper-l  mx-auto shadow-md rounded-sm'>
+                        <img className='image  overflow-hidden ' src={landing3} alt="" />
+                        <div className="middle">
+                            <div className="text flex items-center justify-center gap-1 font-roboto font-light">
+                            <a className='text-sm' target='blank' href="https://mynft-landing-page.vercel.app/">view site</a>
+                            <BiLinkExternal></BiLinkExternal>
+                            </div>
+                        </div>
+                    <div className='overley absolute '></div>
+                </div>
+                <div className=' w-2/3 mx-auto'>
+                    <p className='text-white font-popins text-center py-1 mt-2 text-g '>NFT Landing Page</p>
+                </div>
+               </div>
+               {/* Landing page 4 */}
+
+               <div className='container relative mt-10 md:mt-0'>
+                <div className='image-wrapper-l mx-auto shadow-md rounded-sm'>
+                        <img className='image  overflow-hidden ' src={booking} alt="" />
+                        <div className="middle">
+                            <div className="text flex items-center justify-center gap-1 font-roboto font-light">
+                            <a className='text-sm' target='blank' href="https://booking-app-clone.web.app/">view site</a>
+                            <BiLinkExternal></BiLinkExternal>
+                            </div>
+                        </div>
+                    <div className='overley absolute '></div>
+                </div>
+                <div className=' w-2/3 mx-auto'>
+                    <p className='text-white font-popins text-center py-1 mt-2 text-g '>Booking.com Home page</p>
+                </div>
+               </div>
+
+          
+
             </div>
     );
 };
--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -8,6 +8,7 @@ import { BiLinkExternal } from "react-icons/bi";
 import './Portfolio.css'
 import { Link } from 'react-router-dom';
 import LandingPage from './LandingPage';
+import agency from '../../Images/agency.png'
 
 const Portfolio = () => {
 
@@ -24,7 +25,76 @@ const Portfolio = () => {
             md:left-[366px] bottom-[-3px]'></div>
             </div>
 
-            {/*----------- Projects card --------------*/}
+
+            {/*---------------------- Project Card 1 ------------------ */}
+             <div className='projects-Container md:flex justify-center items-center gap-x-10 md:max-w-[1024px] mx-auto mb-12'>
+                <div className='image-wrapper flex-1 mx-auto md:mx-0'>
+                    <img src={agency} alt="" />
+                </div>
+                <div className='content flex-1'>
+                    <h2 className='project-title2 text-xl text-center md:text-start mt-5 md:mt-0 mx-5 md:mx-0 md:text-2xl text-white mb-3'><span className='font-bold'>Creative Agency </span> A Full-stack &responsive agency website.</h2>
+
+                    <ol className='text-white mt-5  mx-9 md:mx-0 pl-2 md:pl-0'>
+                        <li className='text-white text-sm font-roboto'>1. User can register and order any service</li>
+                        <li className='text-white text-sm font-roboto'>2. Firebase authentication implemented</li>
+                        <li className='text-white text-sm font-roboto'>3. JWT token used to secure API</li>
+                        <li className='text-white text-sm font-roboto'>3. CRUD Operation implemented</li>
+                    </ol>
+
+                    <div className=''>
+                        {/* technology name */}
+                        <div className='flex flex-wrap md:justify-start items-center mt-4 mx-5 md:mx-0 justify-center'>
+                            <div className='flex items-center'>
+                                <FaReact className='text-[#04D3F2]'></FaReact>
+                                <p className=' text-white text-sm mr-2'>React</p>
+                            </div>
+                            <div className='flex items-center'>
+                                <SiTailwindcss className='text-[#7430F8] ml-1'></SiTailwindcss>
+                                <p className=' text-white  text-sm mr-2'>Tailwind CSS</p>
+                            </div>
+                            <div className='flex items-center'>
+                                <SiSwiper className='text-[#0080FF] '></SiSwiper>
+                                <p className=' text-white  text-sm mr-2'>Swiper JS</p>
+                            </div>
+                            <div className='flex items-center'>
+                                <SiTailwindcss className='text-[#7430F8] ml-1'></SiTailwindcss>
+                                <p className=' text-white text-sm mr-2'>Daisy UI</p>
+                            </div>
+                            <div className='flex items-center'>
+                                <FaReact className='text-[#04dcf8] '></FaReact>
+                                <p className=' text-white text-sm mr-2'>React-router</p>
+                            </div>
+                            <div className='flex items-center'>
+                                <SiFirebase className='text-[#FFCB2B]'></SiFirebase>
+                                <p className=' text-white text-sm mr-2'>Firebase</p>
+                            </div>
+                            <div className='flex items-center'>
+                                <SiExpress className='text-[#AFAEAE] ml-1'></SiExpress>
+                                <p className=' text-white text-sm mr-2'>Express Js</p>
+                            </div>
+                            <div className='flex items-center'>
+                                <SiMongodb className='text-[#04B44F]'></SiMongodb>
+                                <p className=' text-white text-sm mr-2'>MongoDB</p>
+                            </div>
+                        </div>
+                        {/*Link  */}
+                        <div className='flex items-center md:justify-start justify-center mt-5 mx-5 md:mx-0 mb-10 md:mb-0'>
+                        <Link  
+                        onClick={() => openInNewTab('https://creative-agency-cyan.vercel.app/')}
+                        className='bg-[#3f37c9] text-white text-xs md:text-sm rounded-lg px-3 py-2'>Live site <BiLinkExternal className='inline mb-1'></BiLinkExternal></Link>
+                        <Link 
+                        onClick={() => openInNewTab('https://github.com/nahid770/creative-agency')}
+                        className='bg-[#3f37c9] text-white text-xs md:text-sm rounded-lg px-3 py-2 ml-2'>Client-side <FaGithub className='inline mb-1'></FaGithub></Link>
+                        <Link 
+                        onClick={() => openInNewTab('https://github.com/nahid770/creative-agency-backend')}
+                        className='bg-[#3f37c9] text-white text-xs md:text-sm rounded-lg px-3 py-2 ml-2'>Server-side <FaGithub className='inline mb-1'></FaGithub></Link>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+
+            {/*----------- Projects card 2--------------*/}
             <div className='projects-Container md:flex justify-center items-center gap-x-10 md:max-w-[1024px] mx-auto'>
                 <div className='image-wrapper flex-1 mx-auto md:mx-0'>
                     <img className='shadow-2xl' src={motoWorld} alt="" />
@@ -35,9 +105,8 @@ const Portfolio = () => {
                     <ol className='text-white mt-5  mx-9 md:mx-0 pl-2 md:pl-0'>
                         <li className='text-white text-sm  font-roboto'>1. Implemented Firebase Authentication</li>
                         <li className='text-white text-sm  font-roboto'>2. Email-Password and Social Media Login/Registration system</li>
-                        <li className='text-white text-sm  font-roboto'>3. Privet Route created. Need to login first to access privet Route</li>
                         <li className='text-white text-sm  font-roboto'>4. CRUD Operation implemented</li>
-                        <li className='text-white text-sm font-roboto'>5. User can Add new Products, Delete products, and Update the stock</li>
+                        <li className='text-white text-sm font-roboto'>5. User can Add, Delete, and Update the stock</li>
                     </ol>
 
                     <div className=''>
@@ -56,8 +125,12 @@ const Portfolio = () => {
                                 <p className=' text-white text-sm mr-2'>React-Bootstrap</p>
                             </div>
                             <div className='flex items-center'>
-                                <FaNodeJs className='text-[#3DA82A] ml-1'></FaNodeJs>
-                                <p className=' text-white text-sm mr-2'>Node Js</p>
+                                <FaReact className='text-[#6930c3] ml-1'></FaReact>
+                                <p className=' text-white text-sm mr-2'>React-router</p>
+                            </div>
+                            <div className='flex items-center'>
+                                <SiFirebase className='text-[#FFCB2B]'></SiFirebase>
+                                <p className=' text-white text-sm mr-2'>Firebase</p>
                             </div>
                             <div className='flex items-center'>
                                 <SiExpress className='text-[#AFAEAE] ml-1'></SiExpress>
@@ -67,14 +140,7 @@ const Portfolio = () => {
                                 <SiMongodb className='text-[#04B44F]'></SiMongodb>
                                 <p className=' text-white text-sm mr-2'>MongoDB</p>
                             </div>
-                            <div className='flex items-center'>
-                                <SiFirebase className='text-[#FFCB2B]'></SiFirebase>
-                                <p className=' text-white text-sm mr-2'>Firebase</p>
-                            </div>
-                            <div className='flex items-center'>
-                                <FaReact className='text-[#6930c3] ml-1'></FaReact>
-                                <p className=' text-white text-sm mr-2'>React-router</p>
-                            </div>
+                           
                         </div>
                         {/*Link  */}
                         <div className='flex items-center md:justify-start justify-center mt-5 mx-5 md:mx-0 mb-10 md:mb-0'>
@@ -92,63 +158,8 @@ const Portfolio = () => {
                     </div>
                 </div>
             </div>
-            {/*---------------------- Project Card ------------------ */}
-            <div className='projects-Container md:flex justify-center items-center gap-x-10 md:max-w-[1024px] mx-auto mt-12'>
-                <div className='image-wrapper flex-1 mx-auto md:mx-0'>
-                    <img src={booking} alt="" />
-                </div>
-                <div className='content flex-1'>
-                    <h2 className='project-title2 text-xl text-center md:text-start mt-5 md:mt-0 mx-5 md:mx-0 md:text-2xl text-white mb-3'><span className='font-bold'>Booking app clone </span> A simple clone of the official Booking.com website.</h2>
-
-                    <ol className='text-white mt-5  mx-9 md:mx-0 pl-2 md:pl-0'>
-                        <li className='text-white text-sm font-roboto'>1. Design booking.com home page</li>
-                        <li className='text-white text-sm font-roboto'>2. Properties page and Login/Registration page</li>
-                        <li className='text-white text-sm font-roboto'>3. Implement searching with date and room opotions</li>
-                    </ol>
-
-                    <div className=''>
-                        {/* technology name */}
-                        <div className='flex flex-wrap md:justify-start items-center mt-4 mx-5 md:mx-0 justify-center'>
-                            <div className='flex items-center'>
-                                <FaReact className='text-[#04D3F2]'></FaReact>
-                                <p className=' text-white text-sm mr-2'>React</p>
-                            </div>
-                            <div className='flex items-center'>
-                                <SiTailwindcss className='text-[#7430F8] ml-1'></SiTailwindcss>
-                                <p className=' text-white  text-sm mr-2'>Tailwind CSS</p>
-                            </div>
-                            <div className='flex items-center'>
-                                <SiSwiper className='text-[#0080FF] ml-1'></SiSwiper>
-                                <p className=' text-white  text-sm mr-2'>Swiper JS</p>
-                            </div>
-                            <div className='flex items-center'>
-                                <SiFirebase className='text-[#FFCB2B]'></SiFirebase>
-                                <p className=' text-white text-sm mr-2'>Firebase</p>
-                            </div>
-                            <div className='flex items-center'>
-                                <FaReact className='text-[#04dcf8] ml-1'></FaReact>
-                                <p className=' text-white text-sm mr-2'>React-router</p>
-                            </div>
-                        </div>
-                        {/*Link  */}
-                        <div className='flex items-center md:justify-start justify-center mt-5 mx-5 md:mx-0 mb-10 md:mb-0'>
-                        <Link  
-                        onClick={() => openInNewTab('https://booking-app-clone.web.app/')}
-                        className='bg-[#3f37c9] text-white text-xs md:text-sm rounded-lg px-3 py-2'>Live site <BiLinkExternal className='inline mb-1'></BiLinkExternal></Link>
-                        <Link 
-                        onClick={() => openInNewTab('https://github.com/nahid770/booking-app-clone')}
-                        className='bg-[#3f37c9] text-white text-xs md:text-sm rounded-lg px-3 py-2 ml-2'>Client-side <FaGithub className='inline mb-1'></FaGithub></Link>
-                        </div>
-                        
-                    </div>
-                </div>
-            </div>
-            {/* ------------------ Project Card -------------------- */}
             
-            {/*------------------------  Project Card  ---------------------*/}
-         
-
-            
+            {/*------------------------  Project Card  3---------------------*/}
              <div className='projects-Container md:flex justify-center items-center gap-x-10 md:max-w-[1024px] mx-auto mt-12 '>
                
                <div className='image-wrapper flex-1 mx-auto md:mx-0'>
@@ -231,14 +242,10 @@ const Portfolio = () => {
             </div>
 
             {/* Box / Container*/}
-            <div className='md:max-w-[1024px] my-10 md:mt-16 content-center mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-y-3 gap-x-5'>
+            <div className=' my-10 md:mt-16 '>
 
-             
                 <LandingPage></LandingPage>
-                
-              
-                
-                
+
             </div>
         </div>
      </div>
